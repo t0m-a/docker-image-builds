7031cc9a2095027f522822dd965eb27763711713
t0m-a <simon_thomas@outlook.fr>
2019-11-28 18:57:20 -0400

    Dockerfiles and directory structures added for LEMP, STATIC and PHP environments

---
+++ compose-php-env/html/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<title>Test Page</title>
+</head>
+<body>
+<h1>HTML Works</h1>
+<p>Your PHP properties are shown below.</p>
+<h1>PHP Works</h1>
+<?php
+phpinfo();
+?>
+</body>
+</html>
